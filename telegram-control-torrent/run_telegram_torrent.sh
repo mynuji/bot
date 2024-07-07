76a5fae4eb81680dbc2f4748526a2d9249c13ea8
igi <myjunu@gmail.com>
2024-07-08 00:03:31 +0900

    Add files via upload

---
+++ telegram-control-torrent/run_telegram_torrent.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#/usr/bin/python3 /home/igi/work/bot/telegram-control-torrent/telegram_torrent.py > /tmp/telegram.log 
+python3 /home/igi/bot/telegram-control-torrent/telegram_torrent.py  &
