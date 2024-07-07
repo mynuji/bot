7069755ed42a333d974f7d92fc342aad2d0dd88f
igi <myjunu@gmail.com>
2024-07-08 00:06:16 +0900

    Update run_telegram_torrent.sh

--- telegram-control-torrent/run_telegram_torrent.sh
+++ telegram-control-torrent/run_telegram_torrent.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#/usr/bin/python3 /home/igi/work/bot/telegram-control-torrent/telegram_torrent.py > /tmp/telegram.log 
-python3 /home/igi/bot/telegram-control-torrent/telegram_torrent.py  &
+python3 /home/userid/bot/telegram-control-torrent/telegram_torrent.py  &
